d0de5f830d10d4f7d8dd20a4bbaa91402adf82f3
Manoj K <47323583+manojmjofficial@users.noreply.github.com>
2019-02-12 20:17:34 +0530

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,13 @@
+//This program checks whether the given number is odd or even.
+import java.util.Scanner;
+class OddEven{
+	public static void main(String[] qwe){
+		Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number:");
+		int a=sc.nextInt();
+		if(a%2==0)
+		System.out.println(a+" is even");
+		else
+		System.out.println(a+" is odd");
+	}
+}
