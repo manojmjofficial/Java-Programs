7a61305e6d9efa14bc9b47d7619a4e47eb0493c6
Manoj K <47323583+manojmjofficial@users.noreply.github.com>
2019-02-12 20:18:38 +0530

    Update OddEven.java

--- OddEven.java
+++ OddEven.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class OddEven{
 	public static void main(String[] qwe){
 		Scanner sc=new Scanner(System.in);
-    System.out.println("Enter a number:");
+    		System.out.println("Enter a number:");
 		int a=sc.nextInt();
 		if(a%2==0)
 		System.out.println(a+" is even");
